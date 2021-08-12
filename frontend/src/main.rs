2f8ecf6730360efc7430e17bb263d092b10e4443
ShadowCurse <shadowcurse57@gmail.com>
2021-08-12 22:04:07 +0000

    updated yew to use async functions

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,6 +4,8 @@ use yew::prelude::*;
 
 enum Msg {
     StartSession,
+    GotMedia(MediaStream),
+    FailedMedia(JsValue),
 }
 
 struct Model {
@@ -23,16 +25,22 @@ impl Component for Model {
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::StartSession => {
-                console::log_1(&"Stargin session".into());
+                console::log_1(&"Starting session".into());
                 console::log_1(&"Initializing video".into());
-                match init_video() {
-                    Ok(_) => {
-                        console::log_1(&"succesfull".into());
+
+                self.link.send_future(async {
+                    match Self::init_video().await {
+                        Ok(md) => Msg::GotMedia(md),
+                        Err(e) => Msg::FailedMedia(e),
                     }
-                    Err(e) => {
-                        console::log_1(&format!("faild with error: {:?}", e).into());
-                    }
-                }
+                });
+            }
+            Msg::GotMedia(media) => {
+                console::log_1(&"successfully".into());
+                console::log_2(&"media_stream: {}".into(), &media);
+            }
+            Msg::FailedMedia(e) => {
+                console::log_1(&format!("failed with error: {:?}", e).into());
             }
         }
         true
@@ -52,15 +60,15 @@ impl Component for Model {
                 <h1 class="uk-heading-medium">{"Web Video Chat in Rust"}</h1>
                 <span class="uk-label">{"Hosting Session ID:"}</span>
                 <h1 class="uk-heading-small">{"Peer A Video"}</h1>
-                <video width="320" height="240" style="color: black; outline-style: solid;" autoplay="muted"></video>
+                <video width="320" height="240" style="color: black; outline-style: solid;" autoplay=false></video>
                 <br/>
                 <button class="uk-button uk-button-default">{"Connect to Session"}</button>
                 <input type="text" class="uk-input"/>
                 <hr/>
                 <h1 class="uk-heading-small">{"Peer B Video"}</h1>
-                <video id="local_video" width="320" height="240" style="color: black; outline-style: solid;" autoplay="muted playsinline"></video>
+                <video id="local_video" width="320" height="240" style="color: black; outline-style: solid;" autoplay=false></video>
                 <br/>
-                <button class="uk-button uk-button-default" onclick=start_session>{"Start Session"}</button>
+                <button class="uk-button uk-button-default" onclick={start_session}>{"Start Session"}</button>
                 <hr/>
                 <button class="uk-button uk-button-default">{"Print Client State"}</button>
                 <button class="uk-button uk-button-default">{"Print Signalling Server State(In Terminal)"}</button>
@@ -69,36 +77,35 @@ impl Component for Model {
     }
 }
 
-fn init_video() -> Result<(), JsValue> {
-    let window = web_sys::window().ok_or("no window found")?;
-    let navigator = window.navigator();
-    let media_device = navigator.media_devices()?;
-    let mut constrains = MediaStreamConstraints::new();
-    constrains.audio(&JsValue::FALSE);
-    constrains.video(&JsValue::TRUE);
-    let stream_promise = media_device
-        .get_user_media_with_constraints(&constrains)?;
+impl Model {
+    async fn init_video() -> Result<MediaStream, JsValue> {
+        let window = web_sys::window().ok_or("no window found")?;
+        let navigator = window.navigator();
+        let media_device = navigator.media_devices()?;
+        let mut constrains = MediaStreamConstraints::new();
+        constrains.audio(&JsValue::FALSE);
+        constrains.video(&JsValue::TRUE);
+        let stream_promise = media_device
+            .get_user_media_with_constraints(&constrains)?;
 
-    let doc = window.document().ok_or("no doc found")?;
-    let video_element = doc
-        .get_element_by_id("local_video")
-        .expect("no local_video element");
-    let video_element = video_element
-        .dyn_into::<HtmlVideoElement>()?;
+        let doc = window.document().ok_or("no doc found")?;
+        let video_element = doc
+            .get_element_by_id("local_video")
+            .expect("no local_video element");
+        let video_element = video_element
+            .dyn_into::<HtmlVideoElement>()?;
 
-    wasm_bindgen_futures::spawn_local(async move {
-        match wasm_bindgen_futures::JsFuture::from(stream_promise).await {
+        let media_stream = match wasm_bindgen_futures::JsFuture::from(stream_promise).await {
             Ok(ms) => {
-                let media_stream = MediaStream::from(ms);
-                video_element.set_src_object(Some(&media_stream));
+                MediaStream::from(ms)
             }
             Err(e) => {
-                console::log_1(&format!("error in setting src object: {:?}", e).into());
+                return Err(format!("error in getting media stream: {:?}", e).into());
             }
-        }
-    });
-
-    Ok(())
+        };
+        video_element.set_src_object(Some(&media_stream));
+        Ok(media_stream)
+    }
 }
 
 fn main() {
