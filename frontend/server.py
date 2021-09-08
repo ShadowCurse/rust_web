b6b60dca98025f768376e018ac862b1994536f36
ShadowCurse <shadowcurse57@gmail.com>
2021-09-08 19:18:19 +0000

    changed to use separate key and cert files

--- frontend/server.py
+++ frontend/server.py
@@ -14,7 +14,8 @@ server_address = ('0.0.0.0', 443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
-                               certfile='../localhost.pem',
+                               certfile='../cert.crt',
+                               keyfile='../key.rsa',
                                ssl_version=ssl.PROTOCOL_TLS)
 print(f"serving server on: https://{server_address[0]}:{server_address[1]}")
 httpd.serve_forever()
